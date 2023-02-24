8cb99b9b7b2000a4ec5a077f70df785e5e4b06a5
azzaz badreddine <azbadr123@gmail.com>
2023-02-25 00:29:43 +0100

    commit test

---
+++ controllers/db_connection.php
@@ -0,0 +1,15 @@
+<!--Database Connection-->
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "articleManagement";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
--- models/Article.php
+++ models/Article.php
@@ -1,5 +1,21 @@
 <?php
 
 class Article{
-    private $
+    private $id;
+    private $title;
+    private $content;
+    private $author;
+    private $publish_date;
+    private $editted_date;
+    private $image;
+
+    public function __construct($id,$title,$content,$author,$publish_date,$editted_date){
+        $this->id=$id;
+        $this->title=$title,
+        $this->content=$content,
+        $this->author=$author,
+        $this->publish_date=$publish_date,
+        $this->editted_date=$editted_date,
+    }
+
 }
